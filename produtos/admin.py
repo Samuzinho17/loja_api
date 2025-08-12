d0f953ccae9e6458cfec5c4eeb0993e1149ea882
Samuzinho17 <SamuelGameplays100@gmail.com>
2025-08-12 16:32:26 -0300

    MEu DEuss, uqi q isso?

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,1 +1,22 @@
+from django.contrib import admin
+from .models import Categoria, Produtos
+from django.utils.html import format_html
 
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+class ProdutoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nome',
+        'categoria',
+        'preço',
+        'estoque',
+        'promoçao',
+        'imagem_preview',
+        'data_criaçao'
+    )
+
+    list_filter = ('categoria', 'promoçao')
+
+    search_fields = ('nome', 'descriçao')
--- produtos/models.py
+++ produtos/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 
-class Produto(models.Model):
-    nome = models.CharField(max_length=200)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    descricao = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nome
-from django.db import models
-
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome
+
+
+class Produtos(models.Model):
+    nome = models.CharField(max_length=200)
+
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    descricao = models.TextField(blank=True, null=True)
+
+    estoque = models.IntegerField()
+
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+
+    promocao = models.BooleanField(default=False)
+
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+from django.db import models
+
